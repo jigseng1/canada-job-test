136175a0458d4a5cdb36b07102fad7b964847cb9
lionelmena <lionelmena@gmail.com>
2013-02-02 06:43:11 +0000

    Calculando el top50 de dominios mas usados, aunque no me parece muy eficiente el algoritmo usado

--- test.pl
+++ test.pl
@@ -5,6 +5,7 @@ use warnings;
 
 use GUI::DB qw(dbConnect query);
 
+# DB TABLES
 our $emails_table = "mailing";
 our $results_table = "domains_count";
 
@@ -53,7 +54,7 @@ sub dailyCount
 }
 
 #
-#Update de table $results_table with the daily domain's counting
+#Updates the table $results_table with the daily domain's counting
 #Parameters: Database handler, reference to the hash of domain's counting
 #Returns: nothing
 #
@@ -66,6 +67,40 @@ sub updateDomainCount
     query($dbh, $sql, %$data);
 }
 
+
+#
+#Calculates the top 50s domains by count
+#Parameters: Database handler,
+#Returns: Array with the top 50 domains
+#
+sub top50
+{
+    my $dbh = shift;
+    my %dtop = ();
+
+    # my $sql = "SELECT  domain, max(daily_count) from $results_table GROUP BY domain";
+    my $sql = "SELECT  domain, daily_count
+               FROM $results_table 
+               ORDER BY daily_count DESC";
+    my @data = query($dbh, $sql);
+
+    foreach my $row (@data)
+    {
+        if (!exists($dtop{$row->{domain}}))
+        {
+            if (scalar(keys(%dtop)) != 50)
+            {
+                $dtop{$row->{domain}} = $row->{daily_count};
+            }
+            else
+            {
+                last;
+            }
+        }
+    }
+
+    return %dtop;
+}
 
 
 my $dbh = dbConnect();
@@ -84,15 +119,23 @@ if (!tableExists($dbh, $results_table))
 
 #Getting daily mails from the $emails_table table 
 $sql = "SELECT addr FROM $emails_table";
-my @data = query($dbh, $sql);
+# my @data = query($dbh, $sql);
 
 #Getting the domains counted
-my %domains_count = dailyCount(\@data);
+# my %domains_count = dailyCount(\@data);
 
 #Updating $results_table with daily emails
-updateDomainCount($dbh, \%domains_count);
+# updateDomainCount($dbh, \%domains_count);
 
-$dbh->disconnect;
+#Getting the top 50 domain
+my %dtop = top50($dbh);
+my @domains = sort {$dtop{$b} <=> $dtop{$a}} (keys(%dtop));
+foreach my $domain (@domains)
+{
+    print "[$domain]: \t $dtop{$domain}\n";
+}
+
+$dbh->disconnect();
 
 #Sorting the domains by it count
 # my @domains = sort {$domains_count{$b} <=> $domains_count{$a}} (keys(%domains_count));
